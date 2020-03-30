adcfb45be00b45a4a49f02cea1268fd89181dc65
이경준 <daniel@igyeongjun-ui-MacBookPro.local>
2020-03-30 23:54:07 +0900

    21, 22번 풀이

--- Study/Study_Q18.java
+++ Study/Study_Q18.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class Study_Q18 {
 
 	// 배열 a의 최댓값을 구하여 반환
-	public static int maxOf(int[] a) {
+	public static int maxOf(final int[] a) {
       int max = a[0];
       for (int i = 1; i < a.length; i++)
          if (a[i] > max)
@@ -12,12 +12,12 @@ public class Study_Q18 {
       return max;
    }
 
-	public static void main(String[] args) {
-      Random rand = new Random();
+	public static void main(final String[] args) {
+      final Random rand = new Random();
 
       System.out.println("키의 최댓값을 구합니다.");
-      int num = rand.nextInt(20);      // 사람 수를 1~20의 랜덤으로 생성
-      int height[] = new int[num];     // 랜덤으로 생성된 num의 수만큼 배열을 생성
+      final int num = rand.nextInt(20);      // 사람 수를 1~20의 랜덤으로 생성
+      final int height[] = new int[num];     // 랜덤으로 생성된 num의 수만큼 배열을 생성
 
       System.out.println("사람수: " + num);
       System.out.println("키 값은 아래와 같습니다.");
---
+++ Study/Study_Q22.java
@@ -0,0 +1,48 @@
+package Study;
+import java.util.Scanner;
+
+public class Study_Q22{
+
+    // 배열 b의 모든 요소를 배열 a에 역순으로 복사하는 메서드 copy를 작성하세요.
+    public static void rcopy(int[] a, int[] b) {
+           for(int i = 0; i < b.length; i++){
+                a[i] = b[b.length - i - 1];                   // b의 요소들을 a에 복사
+            }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        try {
+            System.out.print("b의 길이: ");     // b의 길이 정하기
+            int arr = stdIn.nextInt();
+            int[] b = new int[arr];
+
+            System.out.print("a의 길이: ");     // a의 길이 정하기
+            int arr1 = stdIn.nextInt();
+            int[] a = new int[arr1];
+
+            if( a.length == b.length) {
+
+                for (int i = 0; i < b.length; i++) {
+                    System.out.print("b[" + i + "]: ");     // b의 요소들 정하기
+                    b[i] = stdIn.nextInt();
+                }
+
+                rcopy(a,b);      // 위에서 만든 method 사용
+
+                for (int i = 0; i < a.length; i++) {
+                        System.out.println("a[" + i +"]: " + a[i]); // 결과적으로 a의 요소들 확인
+                }
+
+            } else {
+                System.out.print("a와 b의 길이가 다릅니다."); // a와 b의 길이가 같아야함
+            }
+            
+            
+        }
+        finally {
+            stdIn.close();
+        }
+    }
+}
